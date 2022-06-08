5e255dc0abbca6bd1d75343cc979a1832a03f6f6
Sindre <sindre.glomnes@gmail.com>
2022-06-08 12:24:40 +0200

    TestData

--- Workout/src/main/java/com/sindrgl/Workout/WorkoutApplication.java
+++ Workout/src/main/java/com/sindrgl/Workout/WorkoutApplication.java
@@ -1,7 +1,14 @@
 package com.sindrgl.Workout;
 
+import com.sindrgl.Workout.domain.AppUser;
+import com.sindrgl.Workout.domain.Role;
+import com.sindrgl.Workout.service.AppUserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class WorkoutApplication {
@@ -10,4 +17,27 @@ public class WorkoutApplication {
 		SpringApplication.run(WorkoutApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner run(AppUserService userService) {
+		return arg -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new AppUser(null, "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new AppUser(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("john", "ROLE_MANAGER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
 }
--- Workout/src/main/java/com/sindrgl/Workout/api/AppUserResource.java
+++ Workout/src/main/java/com/sindrgl/Workout/api/AppUserResource.java
@@ -1,13 +1,15 @@
 package com.sindrgl.Workout.api;
 
 import com.sindrgl.Workout.domain.AppUser;
+import com.sindrgl.Workout.domain.Role;
 import com.sindrgl.Workout.service.AppUserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController @RequestMapping("/api")
@@ -19,4 +21,28 @@ public class AppUserResource {
     public ResponseEntity<List<AppUser>>getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser>saveUser(@RequestBody AppUser user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
