7271aefeb01d3f69021a48d980840f149cec799e
Sindre <sindre.glomnes@gmail.com>
2022-06-08 10:55:52 +0200

    Init backend

---
+++ Workout/src/test/java/com/sindrgl/Workout/WorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sindrgl.Workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
