99e7eb1cca416ab1e4d580dfb55f8f0bbae27d57
Sindre <sindre.glomnes@gmail.com>
2022-06-29 18:25:12 +0200

    New workout testing

--- workout_f/src/views/Home.vue
+++ workout_f/src/views/Home.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="wrapper">
     <div id="two">
+      <div>
+        <b-button v-b-modal.modal-prevent-closing>Open Modal</b-button>
+
+        <div class="mt-3">
+          Submitted Names:
+          <div v-if="submittedNames.length === 0">--</div>
+          <ul v-else class="mb-0 pl-3">
+            <li v-for="(name,i) in submittedNames" :key="i">{{ name }}</li>
+          </ul>
+        </div>
+
+        <b-modal
+            id="modal-prevent-closing"
+            ref="modal"
+            title="Submit Your Name"
+            @show="resetModal"
+            @hidden="resetModal"
+            @ok="handleOk"
+        >
+          <form ref="form" @submit.stop.prevent="handleSubmit">
+            <b-form-group
+                label="Name"
+                label-for="name-input"
+                invalid-feedback="Name is required"
+                :state="nameState"
+            >
+              <b-form-input
+                  id="name-input"
+                  v-model="name2"
+                  :state="nameState"
+                  required
+              ></b-form-input>
+            </b-form-group>
+          </form>
+        </b-modal>
+      </div>
       <b-container class="bv-example-row">
         <b-row>
           <b-col>
@@ -61,6 +97,9 @@ export default {
           key: 'reps',
         }
       ],
+      name2: '',
+      nameState: null,
+      submittedNames: []
     }
   },
   created() {
@@ -72,6 +111,35 @@ export default {
       this.workouts = response.data;
       console.log(this.workouts.exercise);
     })
+  },
+  methods: {
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity()
+      this.nameState = valid
+      return valid
+    },
+    resetModal() {
+      this.name = ''
+      this.nameState = null
+    },
+    handleOk(bvModalEvent) {
+      // Prevent modal from closing
+      bvModalEvent.preventDefault()
+      // Trigger submit handler
+      this.handleSubmit()
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return
+      }
+      // Push the name to submitted names
+      this.submittedNames.push(this.name)
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-prevent-closing')
+      })
+    }
   }
 }
 </script>
